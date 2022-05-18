9da77fdeac06c892a22a7a1024c20d273f53c685
ochnios <szymonochnio15@gmail.com>
2022-05-18 20:25:07 +0200

    test button

--- src/main/java/ggapf/Controller.java
+++ src/main/java/ggapf/Controller.java
@@ -1,9 +1,10 @@
 package ggapf;
 
-import java.io.IOException;
 import javafx.fxml.FXML;
 
 public class Controller {
-
-    
+    @FXML
+    private void startButtonAction() {
+        System.out.println("START!!");
+    }
 }
