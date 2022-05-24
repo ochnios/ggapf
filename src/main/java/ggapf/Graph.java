a89c6d6dff732551cd293ed828754495afa70b33
ochnios <szymonochnio15@gmail.com>
2022-05-24 10:38:38 +0200

    code formatted

--- src/main/java/ggapf/Graph.java
+++ src/main/java/ggapf/Graph.java
@@ -6,7 +6,7 @@ import java.util.TreeMap;
 public class Graph {
 	private TreeMap<Integer, TreeMap<Integer, Double>> nodes; // adjacency list
 	private int rows;
-    private int columns;
+	private int columns;
 
 	public Graph(int rows, int columns) {
 		this.nodes = new TreeMap<Integer, TreeMap<Integer, Double>>();
@@ -14,11 +14,11 @@ public class Graph {
 		this.columns = columns;
 	}
 
-    public int getRows() {
+	public int getRows() {
 		return rows;
 	}
 
-    public int getColumns() {
+	public int getColumns() {
 		return columns;
 	}
 
@@ -26,19 +26,19 @@ public class Graph {
 		return nodes.size();
 	}
 
-    public void addEdge(int from, int to, double weight) {
-        if(!(nodes.containsKey(from)))
-			nodes.put(from, new TreeMap<Integer, Double> ()); // add node
+	public void addEdge(int from, int to, double weight) {
+		if (!(nodes.containsKey(from)))
+			nodes.put(from, new TreeMap<Integer, Double>()); // add node
 
-        if(nodes.get(from).containsKey(to))
-            System.out.println("Edge from " + from + " to " + to + " already exists!");
-        else 
-		    nodes.get(from).put(to, weight);
-    }
+		if (nodes.get(from).containsKey(to))
+			System.out.println("Edge from " + from + " to " + to + " already exists!");
+		else
+			nodes.get(from).put(to, weight);
+	}
 
 	public Double getEdgeWeight(int from, int to) {
-		if(nodes.containsKey(from)) {
-			if(nodes.get(from).containsKey(to)) {
+		if (nodes.containsKey(from)) {
+			if (nodes.get(from).containsKey(to)) {
 				return nodes.get(from).get(to);
 			}
 		}
@@ -47,23 +47,24 @@ public class Graph {
 	}
 
 	public TreeMap<Integer, Double> getEdges(int from) {
-		if(nodes.containsKey(from)) {
+		if (nodes.containsKey(from)) {
 			return nodes.get(from);
 		}
 
 		return null;
 	}
 
-    public void print() {
-        System.out.println(rows + " " + columns);
-        for(Map.Entry<Integer, TreeMap<Integer, Double>> row : nodes.entrySet()) {
-            for(Map.Entry<Integer, Double> edge : row.getValue().entrySet()) {
-                System.out.print(edge.getKey() + ":" + edge.getValue() + " ");
-            }
-            System.out.print("\n");
-        }
-    }
+	public void print() {
+		System.out.println(rows + " " + columns);
+		for (Map.Entry<Integer, TreeMap<Integer, Double>> row : nodes.entrySet()) {
+			for (Map.Entry<Integer, Double> edge : row.getValue().entrySet()) {
+				System.out.print(edge.getKey() + ":" + edge.getValue() + " ");
+			}
+			System.out.print("\n");
+		}
+	}
 
 	// TreeMap has removing by key (remove(Object key))
-    // we can add more methods as needed - double getMaxWeight(int from), int getMaxWeightNode(int from), getMin...
+	// we can add more methods as needed - double getMaxWeight(int from), int
+	// getMaxWeightNode(int from), getMin...
 }
